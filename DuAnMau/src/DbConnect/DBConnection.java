d9d3a9817eb3bbdf40614022a4869bf836275b78
hoaquangsy <hoaquangsy24@gmail.com>
2021-10-03 11:19:50 +0700

    sy
    design

---
+++ DuAnMau/src/DbConnect/DBConnection.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DbConnect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Admin
+ */
+public class DBConnection {
+
+    public Connection getConnection() throws Exception {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String url = "jdbc:sqlserver://localhost;databaseName=Assigment;user=sa;password=1";
+        return  DriverManager.getConnection(url);
+    }
+
+
+}
